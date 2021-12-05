22e1d3d166588043037eb61fe665715f6022205a
Gecko05 <jacacent@gmail.com>
2021-12-05 10:22:19 -0600

    Working UART Demo with 7 segment display, and UART RX/TX loop

--- UART/UART_Display.vhd
+++ UART/UART_Display.vhd
@@ -33,6 +33,7 @@ entity UART_Display is
 	PORT (
 			CLK_100MHz : IN STD_LOGIC;
 			DEV_UART_RX : IN STD_LOGIC;
+			DEV_UART_TX : OUT STD_LOGIC;
 			o_LED : OUT STD_LOGIC_VECTOR(0 TO 2);
 			SevenSegment : OUT STD_LOGIC_VECTOR(0 TO 7);
 			SevenSegmentEnable : OUT STD_LOGIC_VECTOR(0 TO 2)
@@ -62,6 +63,22 @@ architecture Behavioral of UART_Display is
 			signal i_DATA : STD_LOGIC_VECTOR(7 DOWNTO 0)
 		   );
 	 END COMPONENT;
+	 
+	COMPONENT UART_TX
+	PORT(
+		i_Clk : IN std_logic;
+		i_TX_DV : IN std_logic;
+		i_TX_Byte : IN std_logic_vector(7 downto 0);          
+		o_TX_Active : OUT std_logic;
+		o_TX_Serial : OUT std_logic;
+		o_TX_Done : OUT std_logic
+		);
+	END COMPONENT;
+	
+	signal TX_DV : STD_LOGIC := '0';
+	signal TX_BYTE : STD_LOGIC_VECTOR(7 DOWNTO 0) := X"00";
+	signal TX_Active : STD_LOGIC := '0';
+	signal TX_Done : STD_LOGIC := '0';
 begin
 	RX_MODULE: UART_RX PORT MAP (
 		 i_RX_Serial => DEV_UART_RX,
@@ -76,6 +93,14 @@ begin
 		 o_SevenSegmentEnable => SevenSegmentEnable,
 		 i_DATA => RX_DATA
 	);
-
+	
+	TX_MODULE: UART_TX PORT MAP(
+		i_Clk => CLK_100MHz,
+		i_TX_DV => RX_DV,
+		i_TX_Byte => RX_DATA,
+		o_TX_Active => TX_Active,
+		o_TX_Serial => DEV_UART_TX,
+		o_TX_Done => TX_Done
+	);
 end Behavioral;
 
